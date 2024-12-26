6cc7daf5b202e11e8564d9143728181b792785ef
jinson jijy <jinsonjijy50@gmail.com>
2024-12-26 14:21:21 +0530

    made a what'sapp ui with the listtile and listview where we used the listile has 4 components it is used inside the listview

--- lib/screen_home.dart
+++ lib/screen_home.dart
@@ -13,12 +13,20 @@ class _ScreenHomeState extends State<ScreenHome> {
     return Scaffold(
       body: Container(
               child: ListView.separated(itemBuilder: (ctx,index){
-                return Text("person $index");
+                return ListTile(
+                    title: Text('person $index'),
+                    subtitle: Text("hello how are you"),
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyy4Udb9Yx3ntXPhpR7-dMzicNXGD1YmOsvA&s'),
+                    ),
+                    trailing: Text('$index:30 pm'),
+                );
               },
                separatorBuilder: (ctx,index){
-               return Text('this is a seperator');
+               return Divider();
                },
-               itemCount: 50
+               itemCount: 30
                ),
       ),
     );
